9901602e94c0bcd94c31472dc6b10f5a1e5aa665
Brian G <brigeo987@gmail.com>
2024-07-16 16:42:42 -0400

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -2,95 +2,111 @@
  * Global Variables
  */
 
-//Setting the cells to a node list
+//Source code: https://www.youtube.com/watch?v=AnmwHjpEhtA
 const cells = document.querySelectorAll('.cell');
+const statusText = document.querySelector("#display_turn");
+const startBtn = document.querySelector("startBtn");
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]; //end winConditions
+let gameBoard = [
+    "","","",
+    "","","",
+    "","",""
+]
+let computerPlayer = "X";
+let playerSymbols = ["X", "O"];
+let selection = playerSymbols[Math.random(0,2)];
+let currentPlayer = "O";
+// let playerTwo = "XX";
+let gameRunning = false;
 
-//Adding event listener
-cells.forEach(cell => {
-    cell.addEventListener('click', clickedCell, false);
-}); //end event listener
+initializeGame();
 
-/**
+function initializeGame() {
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));    
+    statusText.textContent = `${computerPlayer}'s turn!`;
+    gameRunning = true;
+}
+
+function cellClicked() {
+    const cellIndex = this.getAttribute("cell-index");
+
+    if (gameBoard[cellIndex] != "" || !gameRunning){
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+
+function updateCell(cell, index){
+    gameBoard[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+} 
+
+function changePlayer() {
+    
+    currentPlayer = (currentPlayer == "O") ? "XX" : "O";
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+
+function checkWinner() {
+    let roundWon = false;
+    for (let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        const cellA = gameBoard[condition[0]];
+        const cellB = gameBoard[condition[1]];
+        const cellC = gameBoard[condition[2]];
+
+        if(cellA == "" || cellB =="" || cellC == "") {
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC) {
+            roundWon = true;
+            break;
+        }
+    } //end for loop
+
+    if(roundWon) {
+        statusText.textContent = `${currentPlayer} won!`;
+        gameRunning = false;
+    } else if(!gameBoard.includes("")) {
+        statusText.textContent = "Draw!";
+    } else {
+        changePlayer();
+    }
+    startBtn.innerHTML = "Restart Game?";
+} //end checkWinner
+
+function restartGame() {
+    gameBoard = [
+        "","","",
+        "","","",
+        "","",""
+    ]
+    statusText.textContent ="Start the game to play!";
+    cells.forEach(cell => cell.textContent = "")
+    gameRunning = true
+}
+
+
+ /**
  * Timer Functionality
  */
 
-/**
- * Game Logic 
- */
-
-// const NUM_ROWS = 3;
-// const NUM_COLS = 3;
-
-let gameBoard = [
-    ['','',''],
-    ['','',''],
-    ['','','']
-]
 
 
-//Check to see if there is a winner or a tied game
-function checkGameState(){
-    for (var i = 0; i < gameBoard.length; i++ ) {
-        for (var j = 0; j < gameBoard[i].length; j++) {
-            var rows = gameBoard[i];
-            var cols = gameBoard[j];
-            //Placeholder logic. Switch out 'cols' for whatever variable you set the players' symbols to.
-            if (rows === cols) {
-                //Whichever player that is wins horizontally
-                //Again, replace rows with players' variable
-            } else if (cols === rows) {
-                //Winning vertically
-            } else if (cols % 2 && cols === rows) {
-                //Winning diagonally
-            } else if (cols !== '' && rows !== '') {
-                //Cat's game/tie
-            }
-        }
-    }
-}
 
-//1. Check to see if the game is one or two player (using the radio buttons in the HTML)
-function determineNumOfPlayers(isSinglePlayer) {
-    let playerSymbols = ['X','O'];
-    let assignSymbol = Math.random(0,2);
-    let numberOfPlayers = 1;
-    let playerOneSymbol;
-    let playerTwoSymbol;
-    
 
-    if (isSinglePlayer === true) {
-         playerTwoSymbol = playerSymbols[0];
-         playerOneSymbol = playerSymbols[1];
-    } else {
-        playerOneSymbol = playerSymbols[assignSymbol];
-        playerTwoSymbol = playerSymbols[assignSymbol];
-    }
-}
 
-function gameplayLogic(){
-//2. Assign either X or O to each player using Math.random()
-    //2.1 If it is two player, they must altertnate taking turns
-    //2.2 If it is single player the "AI" will always be X and the player will always be O
-        //Using Math.random() decide who goes first (just like in two player)
-        //Computer moves using Math.random() to determine where to place its symbole IN AN EMPTY SPACE
-//3. Record and display the amount of time the game has been played for
-//4. Once the game is over:
-    //4.1 Determine if there is a winner. Display the winner
-    //4.2 Determine if there is NO winner (cat's game)
-    //4.3 Stop the timer
-//5. After the game state has been decided
-    //5.1 Ask the user if they would like to play again (Button should change to 'Restart')
-    //5.2 The "Play Again" button should be available only when the game is over (modal?)
-    //5.3 Display the outcome of the game (Winner! Loser :( or Tied)
 
-}
 
-/**
- * Start the game on window finished loading
- */
 
-function start(){
-    //NYI
-}
-
-window.addEventListener('load', start);
